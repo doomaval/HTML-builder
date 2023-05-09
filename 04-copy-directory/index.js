0575fcf3ccb8aa5db058b32c5082a857ad0b12c9
Valeriy Duma <doomaval@gmail.com>
2023-05-09 22:51:08 +0300

    feat: Task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,18 +3,28 @@ const path = require("path");
 const source = path.join(__dirname, "/files/");
 const destination = path.join(__dirname, "/files-copy/");
 
+// fs.rm(destination, { recursive: true }).fs.mkdir(destination, {
+//   recursive: true,
+// });
+
+// const list = fs.readdir(source);
+// console.log(list);
+// for (let item of list) {
+//   fs.copyFile(path.join(source, item), path.join(destination, item));
+// }
+
 copyFolder();
 
 async function copyFolder() {
   try {
-    fs.rm(destination, { recursive: true });
+    await fs.rm(destination, { recursive: true, force: true });
+    await fs.mkdir(destination, { recursive: true });
+    const list = await fs.readdir(source);
+    // console.log(list);
+    for (let item of list) {
+      fs.copyFile(path.join(source, item), path.join(destination, item));
+    }
   } catch (err) {
-    // console.error(err);
-  }
-  fs.mkdir(destination, { recursive: true });
-  const list = await fs.readdir(source);
-  console.log(list);
-  for (let item of list) {
-    fs.copyFile(path.join(source, item), path.join(destination, item));
+    console.error(err);
   }
 }

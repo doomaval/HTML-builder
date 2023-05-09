762e726c8c6ab12bd0dfe0043442cc15b4752b61
Valeriy Duma <doomaval@gmail.com>
2023-05-09 22:53:03 +0300

    refactor: Task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,16 +3,6 @@ const path = require("path");
 const source = path.join(__dirname, "/files/");
 const destination = path.join(__dirname, "/files-copy/");
 
-// fs.rm(destination, { recursive: true }).fs.mkdir(destination, {
-//   recursive: true,
-// });
-
-// const list = fs.readdir(source);
-// console.log(list);
-// for (let item of list) {
-//   fs.copyFile(path.join(source, item), path.join(destination, item));
-// }
-
 copyFolder();
 
 async function copyFolder() {
